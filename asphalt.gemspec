8a9071c2e437fe4243b255ea2abd50224ce18f82
Don Okuda <don.okuda@gmail.com>
2013-08-04 16:20:44 -0700

    Add `init!` method for generating bare files and directories

--- asphalt.gemspec
+++ asphalt.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Asphalt::VERSION
   spec.authors       = ["Don Okuda"]
   spec.email         = ["don.okuda@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = "Asphalt generates an opinionized directory structure for your Sass files"
+  spec.summary       = "Asphalt generates an opinionized directory structure for your Sass files"
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec", "~> 2.14.1"
 end
---
+++ spec/asphalt_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Asphalt do
+  describe "#init!" do
+    before(:all) do
+      @temp_directory = Dir.mktmpdir
+      puts "Temp directory: #{ @temp_directory }"
+    end
+
+    it "creates the correct set of directories and files" do
+      Asphalt.init!(@temp_directory)
+
+      File.should exist(File.join(@temp_directory, 'main.scss'))
+
+      Dir.should exist(File.join(@temp_directory,  'modules'))
+      File.should exist(File.join(@temp_directory, 'modules', '_all.scss'))
+      File.should exist(File.join(@temp_directory, 'modules', '_utility.scss'))
+      File.should exist(File.join(@temp_directory, 'modules', '_colors.scss'))
+
+      Dir.should exist(File.join(@temp_directory,  'partials'))
+      File.should exist(File.join(@temp_directory, 'partials', '_base.sass'))
+      File.should exist(File.join(@temp_directory, 'partials', '_buttons.scss'))
+      File.should exist(File.join(@temp_directory, 'partials', '_figures.scss'))
+      File.should exist(File.join(@temp_directory, 'partials', '_grids.scss'))
+      File.should exist(File.join(@temp_directory, 'partials', '_typography.scss'))
+      File.should exist(File.join(@temp_directory, 'partials', '_reset.scss'))
+
+      Dir.should exist(File.join(@temp_directory, 'vendors'))
+    end
+
+
+    after(:all) do
+      FileUtils.remove_entry_secure @temp_directory
+    end
+  end
+end
