f4f96a38e37c65678396935f1ce511d27b7e55fa
Don Okuda <don.okuda@gmail.com>
2014-10-07 23:47:30 -0700

    Add `.get_main_stylesheet` method

--- lib/asphalt/config.rb
+++ lib/asphalt/config.rb
@@ -5,4 +5,9 @@ module Asphalt::Config
     config_hash = YAML.load(config)
     config_hash.fetch("stylesheets_directory")
   end
+
+  def self.get_main_stylesheet(config)
+    config_hash = YAML.load(config)
+    config_hash.fetch("main_stylesheet")
+  end
 end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -13,3 +13,17 @@ describe Asphalt::Config, ".get_stylesheets_directory"  do
     expect(result).to eq "app/assets/stylesheets"
   end
 end
+
+describe Asphalt::Config, ".get_main_stylesheet"  do
+  before(:each) do
+    @config = <<-YAML
+      main_stylesheet: application.scss
+    YAML
+    @config.gsub!(/^\s{6}/, "")
+  end
+
+  it "retrieves the filename of the main stylesheet" do
+    result = Asphalt::Config.get_main_stylesheet(@config)
+    expect(result).to eq "application.scss"
+  end
+end
